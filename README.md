40643197feb60a81d6b685332d066c8d820cd1fe
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-07-23 17:10:00 -0300

    Somas

--- README.md
+++ README.md
@@ -9,16 +9,21 @@ sim
 
 - git status -> verifica a situação da branch local em relação a remota
 
-- git add . -> adiciona os arquivos alterados e adicionados
+- git add . -> adiciona os arquivos alterados e adicionados (ser usado primeiro no terminal do VS code)
 
-- git commit -m "nome do commit" -> nome e rastreio da alteração
+- git commit -m "nome do commit" -> nome e rastreio da alteração(ser usado na segunda)
 
-- git push -> alinamento entre remoto e local
+- git push -> alinamento entre remoto e local(ser usado na terceira)
 
 - git push origin <nomeDaBranch>, ex: git push origin main
 
 # comandos cli
 
 - cd<nomeDoCaminho>/ cd../ ls , ex: cd Desktop/
+
+
+# JavaScript
+
+- 
 
  
--- Task01/script.js
+++ Task01/script.js
@@ -1,2 +1,71 @@
+function somar(){
+    var n1 = parseFloat(document.getElementById("n1").value);
+    var n2 = parseFloat(document.getElementById("n2").value);
 
-alert("Ja que é string escrevo #$%¨&**((*")
+    var resp = document.getElementById("respSoma")
+    resp.textContent = n1 + n2;
+}  
+
+function subtrair(){
+    var n1 = parseFloat(document.getElementById("n1-").value);
+    var n2 = parseFloat(document.getElementById("n2-").value);
+
+    var resp = document.getElementById("respSub")
+    resp.textContent = n1 - n2;
+}
+
+
+function dividir(){
+    var n1 = parseFloat(document.getElementById("n1/").value);
+    var n2 = parseFloat(document.getElementById("n2/").value);
+
+    var resp = document.getElementById("respDiv")
+    resp.textContent = n1 / n2;
+
+}
+
+function multiplicar(){
+    var n1 = parseFloat(document.getElementById("n1*").value);
+    var n2 = parseFloat(document.getElementById("n2*").value);
+
+    var resp = document.getElementById("respMul")
+    resp.textContent = n1 * n2;
+
+
+}
+
+function media(){
+    var m1 = parseFloat(document.getElementById("m1").value);
+    var m2 = parseFloat(document.getElementById("m2").value);
+    var m3 = parseFloat(document.getElementById("m3").value);
+
+    var media = document.getElementById("respMed");
+    var res = (m1 + m2 + m3)/3;
+
+    if(res >= 7){
+        media.textContent = res.toFixed(1)
+        media.style.color = "green";
+        //media.style.backgroundColor = "#65D186"; ---> função para mudar a cor de fundo da fonte
+        //media.style.padding = "5px"  ---> função para aumentar o tamanho da fonte
+    }
+    else{
+        media.textContent = res.toFixed(1);
+        media.style.color = "red";
+    }
+
+}
+
+function contar(){
+    const num = parseFloat($("#numberInput").val());
+
+    var resultadoStr = "";
+
+    if(num <= 100){
+        for(var cont = 1; cont <= num; cont++){
+            resultadoStr = resultadoStr + cont + "<br>";
+        }
+        $("#result").html(resultadoStr);
+    }else{
+        $("#result").text("Digite um número menor que 100!!!");
+    }
+}
